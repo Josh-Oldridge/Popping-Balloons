8fda377c1efb218edd8737d8927e30570f027a4a
Josh <oldridgejoshua@gmail.com>
2023-11-09 22:43:01 +0100

    v

--- popBalloons/popBalloons.cpp
+++ popBalloons/popBalloons.cpp
@@ -15,25 +15,48 @@ using namespace glm;
 #include <common/shader.hpp>
 
 #include <vector>
+#include <random>
+#include <algorithm>
+
+#include <glm/gtc/matrix_transform.hpp> // for glm::ortho
+
 
 // Define a structure for Vertex data
 
-struct Vertex {
-    glm::vec3 position;
-    glm::vec3 color;
-};
 
 // Initialize a shader program
 GLuint programID;
 // Initialize Vertex Array Object and Vertex Buffer Object
 GLuint VAO, VBO;
 
-PopBalloons::PopBalloons() {
-    // Constructor logic, set up any initial values
+PopBalloons::PopBalloons()
+    : lastTime(glfwGetTime()), // Initialize lastTime to current time
+      nextBalloonTime(0), 
+      gen(std::random_device{}()), 
+      score(0), 
+      lives(3) 
+{
+    nextBalloonTime = lastTime + generateRandomTime(); // Set initial timer using lastTime
+    // ... other initialization code ...
 }
 
 PopBalloons::~PopBalloons() {
     // Destructor logic, clean up resources
+}
+
+// Inside PopBalloons class in popBalloons.cpp
+float PopBalloons::generateRandomTime() {
+    std::uniform_real_distribution<float> disTime(2.0f, 5.0f); // Between 2 and 5 seconds
+    return disTime(gen); // Assuming gen is a std::mt19937 already defined
+}
+
+// Method to handle balloon popping
+void PopBalloons::popBalloon(int balloonIndex) {
+    Balloon& balloon = balloons[balloonIndex];
+    // Calculate score based on size (smaller balloon gives higher score)
+    score += static_cast<int>((0.15f - balloon.getSize()) * 100.0f);
+    // Remove balloon from the vector
+    balloons.erase(balloons.begin() + balloonIndex);
 }
 
 void PopBalloons::Run() {
@@ -45,10 +68,18 @@ void PopBalloons::Run() {
 
     setupScene();
 
+    // Initialize lastTime to the current time
+    lastTime = glfwGetTime();
+
     // This is your main loop
     while (!glfwWindowShouldClose(window)) {
-        // Update the scene
-        updateScene();
+        // Calculate the time elapsed since the last frame
+        double currentTime = glfwGetTime();
+        float deltaTime = static_cast<float>(currentTime - lastTime);
+        lastTime = currentTime; // Update lastTime for the next frame
+
+        // Update the scene with the calculated deltaTime
+        updateScene(deltaTime);
 
         // Render the scene
         renderScene();
@@ -75,12 +106,16 @@ bool PopBalloons::initializeGLFW() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+    // Get the framebuffer size instead of window size in case of high-DPI screens
+    
+    
     return true;
 }
 
 bool PopBalloons::initializeWindow() {
     // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(640, 480, "Pop Balloons", NULL, NULL);
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    window = glfwCreateWindow(mode->width, mode->height, "Pop Balloons", glfwGetPrimaryMonitor(), NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
@@ -89,6 +124,10 @@ bool PopBalloons::initializeWindow() {
 
     // Make the window's context current
     glfwMakeContextCurrent(window);
+
+    glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+    glViewport(0, 0, fbWidth, fbHeight);
+
 
     return true;
 }
@@ -113,8 +152,19 @@ bool PopBalloons::initializeGLEW() {
 }
 
 void PopBalloons::setupScene() {
-    //Create and compile our GLSL program from the shaders
-    programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+    // Create and compile our GLSL program from the shaders
+    programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+    // Get the framebuffer size
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    float aspectRatio = static_cast<float>(screenWidth) / static_cast<float>(screenHeight);
+    glm::mat4 projection = glm::ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f);
+
+    // Pass the orthographic projection matrix to the shader
+    GLuint matrixID = glGetUniformLocation(programID, "uProjectionMatrix");
+    glUseProgram(programID);
+    glUniformMatrix4fv(matrixID, 1, GL_FALSE, &projection[0][0]);
 
     // Define vertices for a filled circle (balloon)
     std::vector<Vertex> vertices;
@@ -123,25 +173,14 @@ void PopBalloons::setupScene() {
     unsigned int num_segments = 32; // Increase for a smoother circle
 
     // Add the center vertex for the triangle fan
-    vertices.push_back({glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f)}); // Center vertex, red color
+    vertices.emplace_back(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f);
 
     // Loop to create vertices around the center vertex
     for (unsigned int i = 0; i <= num_segments; i++) {
         float theta = 2.0f * 3.1415926f * float(i) / float(num_segments); // Current angle
-
-        Vertex v;
-
-        // Calculate position for vertices to create an upright oval
-        v.position.x = horizontal_radius * cosf(theta); // Use horizontal radius for x-axis
-        v.position.y = vertical_radius * sinf(theta);   // Use vertical radius for y-axis
-        v.position.z = 0.0f;
-
-        // Set color to red
-        v.color.r = 1.0f;
-        v.color.g = 0.0f;
-        v.color.b = 0.0f;
-
-        vertices.push_back(v);
+        float px = horizontal_radius * cosf(theta);
+        float py = vertical_radius * sinf(theta);
+        vertices.emplace_back(glm::vec3(px, py, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f);
     }
     
 
@@ -156,36 +195,58 @@ void PopBalloons::setupScene() {
     // Give our vertices to OpenGL
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
-    // 1st attribute buffer : vertices
+    // 1st attribute buffer : vertices (2D positions here)
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(
-        0,              //attribute. No particular reason for 0, but must match the layout in the shader
-        3,              //size of the vertex arrribute
-        GL_FLOAT,       //type
-        GL_FALSE,       //normalized
-        sizeof(Vertex), //stride
-        (void*)0        //array buffer offset
-    );
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 
     // 2nd attribute buffer : colors
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(
-        1,                              // attribute. Matches layout in shader
-        3,                              // size
-        GL_FLOAT,                       // type
-        GL_FALSE,                       // normalized
-        sizeof(Vertex),                 // stride
-        (void*)offsetof(Vertex, color)  // array buffer offset
-    );
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, r)));
 
-    // Unbind the VAO for now to avoid accidental modification
+
+    // Unbind the VAO
     glBindVertexArray(0);
 }
 
-void PopBalloons::updateScene() {
-    // Update your scene (move balloons, check for popping, etc.)
+void Balloon::update(float deltaTime) {
+    position.y += 5 * deltaTime; // Adjust `movementSpeed` based on how fast you want balloons to move
 }
 
+void PopBalloons::updateScene(float deltaTime) {
+    // Update each balloon using deltaTime
+    for (auto& balloon : balloons) {
+        balloon.update(deltaTime);
+    }
+
+    // Check if any balloons are off-screen and remove them
+    for (size_t i = 0; i < balloons.size();) {
+        if (balloons[i].isOffScreen()) {
+            balloons.erase(balloons.begin() + i);
+            --lives;
+            // Game over logic
+            if (lives <= 0) {
+                endGame();
+                return; // Stop the update loop because the game is over
+            }
+        } else {
+            ++i;
+        }
+    }
+
+    // Check if it's time to create a new balloon
+    float currentTime = glfwGetTime();
+    if (currentTime >= nextBalloonTime) {
+        createBalloon(); // Create a new balloon at random position and color
+        nextBalloonTime = currentTime + generateRandomTime(); // Set the time for creating the next balloon
+    }
+}
+
+void PopBalloons::endGame() {
+    std::cout << "Game Over! Your score: " << score << std::endl;
+    // Perform any additional cleanup and exit the game
+    // For example:
+    glfwSetWindowShouldClose(window, 1); // Signal to close the game window
+}
 
 
 
@@ -205,15 +266,61 @@ void PopBalloons::renderScene() {
     // Bind the VAO
     glBindVertexArray(VAO);
 
-   
-    glDrawArrays(GL_TRIANGLE_FAN, 0, num_segments + 2); 
+    for (const auto& balloon : balloons) {
+        std::vector<Vertex> vertices = createBalloonVertices(balloon);
 
-    
+        // Update VBO data
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(Vertex), &vertices[0]);
 
-    // Unbind the VAO
+        // Draw the balloon
+        glDrawArrays(GL_TRIANGLE_FAN, 0, vertices.size());
+    }
+
     glBindVertexArray(0);
-
 }
+void PopBalloons::createBalloon() {
+    static std::mt19937 gen{ std::random_device{}() };
+    std::uniform_real_distribution<float> dis(-1.0, 1.0); // For x-coordinate
+    std::uniform_real_distribution<float> disColor(0.0, 1.0); // For color
+    std::uniform_real_distribution<float> disSize(0.05f, 0.15f); // For size
+
+    glm::vec3 position(dis(gen), -1.0f, 0.0f); // Start from the bottom of the screen
+    glm::vec3 color(disColor(gen), disColor(gen), disColor(gen)); // Random color for each balloon
+    float size = disSize(gen); // Random size for each balloon
+
+    balloons.push_back(Balloon(position, size, color));
+}
+
+std::vector<Vertex> PopBalloons::createBalloonVertices(const Balloon& balloon) {
+    std::vector<Vertex> vertices;
+    
+    // Retrieve position and color from the Balloon object
+    glm::vec3 position = balloon.getPosition(); // Get the balloon position
+    glm::vec3 color = balloon.getColor(); // Get the balloon color
+    
+    // Constants to define the oval shape and number of segments
+    float horizontal_radius = balloon.getSize() * 0.5f; // Example horizontal size
+    float vertical_radius = balloon.getSize(); // Example vertical size
+    unsigned int num_segments = 20; // Increase for smoother circle (balloon)
+    
+    // Center vertex for triangle fan (we won't need a Z-coordinate for 2D rendering)
+    vertices.emplace_back(position, color, 1.0f);
+    
+    // Loop to create vertices around the center
+    for (unsigned int i = 0; i <= num_segments; ++i) {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments); // Current angle
+        
+        // Calculate position for vertices to create an upright oval
+        float px = position.x + horizontal_radius * cosf(theta);
+        float py = position.y + vertical_radius * sinf(theta);
+        
+        vertices.emplace_back(glm::vec3(px, py, 0.0f), color, 1.0f);
+    }
+    
+    return vertices;
+}
+
 void PopBalloons::cleanup() {
     // Cleanup VBO and shader
     glDeleteBuffers(1, &VBO);
--- popBalloons/popBalloons.h
+++ popBalloons/popBalloons.h
@@ -8,8 +8,42 @@
 #include <glm/glm.hpp>
 // Include GLFW
 #include <glfw3.h>
+#include <vector>
+#include <random>
 
 // Define any global variables, constants, and structs for your balloon objects here
+
+struct Vertex {
+    float x, y;  // Position in 2D space
+    float r, g, b, a;  // Color in RGBA format
+
+    Vertex(glm::vec3 pos, glm::vec3 col, float alpha)
+        : x(pos.x), y(pos.y), r(col.r), g(col.g), b(col.b), a(alpha) {}
+};
+
+class Balloon {
+private:
+    glm::vec3 position;
+    float size;
+    glm::vec3 color;
+
+public:
+    // Constructor
+    Balloon(const glm::vec3& pos, float sz, const glm::vec3& col)
+        : position(pos), size(sz), color(col) {}
+
+    // Getter methods
+    glm::vec3 getPosition() const { return position; }
+    float getSize() const { return size; }
+    glm::vec3 getColor() const { return color; }
+
+    void update(float deltaTime);
+
+    // Check if the balloon is off the screen
+    bool isOffScreen() const {
+        return position.y > 1.0f;
+    }
+};
 
 class PopBalloons {
 public:
@@ -17,8 +51,14 @@ public:
     ~PopBalloons(); // Destructor
     
     void Run(); // Main function to run the game loop
+    void popBalloon(int balloonIndex);
+    void endGame();
+    void updateScene(float deltaTime); // Add deltaTime parameter
+
 
 private:
+    int score;
+    int lives;
     bool initializeGLFW();
     bool initializeWindow();
     bool initializeGLEW();
@@ -26,13 +66,24 @@ private:
     void updateScene();
     void renderScene();
     void cleanup();
+    std::vector<Balloon> balloons;
+    void createBalloon();
+    std::vector<Vertex> createBalloonVertices(const Balloon& balloon);
+    float nextBalloonTime; // The time when the next balloon should be created
+    float generateRandomTime();
+    std::mt19937 gen; // The random number generator instance
+    double lastTime; // Time of the last frame
+
     
     // Add your variables for the window, shaders, and buffers here
     GLFWwindow* window;
     GLuint programID;
+    int fbWidth, fbHeight;
     // More variables like buffers, array objects, etc.
 
     // Add any other private member functions or variables needed
 };
+
+
 
 #endif // POPBALLOONS_H
